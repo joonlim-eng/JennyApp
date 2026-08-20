9db491f9995dd46f006aca4d30860e61a632e1df
Hyun Joon Lim <joonlim@jenybs.com>
2026-08-19 21:19:56 -0500

    d

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,13 @@ module.exports = {
       name: "expo-metro",
       script: "cmd.exe",
       args: "/c npx expo start --tunnel -c",
+      windowsHide: true,
       autorestart: true
     },
     {
       name: "git-autopull",
       script: "auto-pull.js",
+      windowsHide: true,
       autorestart: true
     }
   ]
