2a736b50b7eead42a063e49d9a979301218bf18c
Hyun Joon Lim <joonlim@jenybs.com>
2026-08-19 22:40:08 -0500

    update

--- metro.js
+++ metro.js
@@ -1,11 +1,11 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
-// { windowsHide: true } 를 넣어야 화면에 QR코드 창이 새로 뜨지 않고 백그라운드에 숨습니다.
-const metro = exec('npx expo start --tunnel -c', { windowsHide: true });
-
-metro.stdout.on('data', (data) => console.log(data));
-metro.stderr.on('data', (data) => console.error(data));
-
-metro.on('exit', (code) => {
-  console.log(`Metro process exited with code ${code}`);
+// --non-interactive 옵션을 추가하여 백그라운드 대기 상태(CommandError)를 방지합니다.
+const metro = spawn('npx.cmd', ['expo', 'start', '--tunnel', '--non-interactive', '-c'], {
+  windowsHide: true,
+  shell: true
 });
+
+metro.stdout.on('data', (data) => console.log(data.toString()));
+metro.stderr.on('data', (data) => console.error(data.toString()));
+metro.on('close', (code) => console.log(`Metro process exited with code ${code}`));
