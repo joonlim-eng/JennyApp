dc5798c9d11cb87899048376c11acf67f87c8c7c
Hyun Joon Lim <joonlim@jenybs.com>
2026-09-05 14:23:01 -0500

    Sdgads

--- metro.js
+++ metro.js
@@ -1,7 +1,7 @@
 const { spawn } = require('child_process');
 
 // CI=1 환경변수를 주입하여 백그라운드 대기 및 터널 키보드 입력을 자동으로 우회합니다.
-const metro = spawn('npx.cmd', ['expo', 'start', '--tunnel', '-c'], {
+const metro = spawn('npx.cmd', ['expo', 'start', '--tunnel', '--web', '-c'], {
   windowsHide: true,
   shell: true,
   env: { ...process.env, CI: '1' }
